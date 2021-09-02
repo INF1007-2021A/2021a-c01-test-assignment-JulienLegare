7cfdb40cc2236e4b3f29c9dd8c3b8792e793de0b
JulienLegare <julien.legare@polymtl.ca>
2021-09-02 15:11:52 -0400

    Ajoute une fonction

--- exercice.py
+++ exercice.py
@@ -4,3 +4,10 @@
 if __name__ == '__main__':
     #TODO: Modifier le code ici
     print("Le cours INF1007 est mon cours")
+    def exercice():
+        if 2==2:
+        print("exercice reussi")
+        else:
+        print("oh no")
+
+    exercice()
